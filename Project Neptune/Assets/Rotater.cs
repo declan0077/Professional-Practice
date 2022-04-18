35e671fdd742bb2752aaa765b9388822704a9c08
declan0077 <icetheicebear@gmail.com>
2022-04-18 16:13:35 +0100

    Bouncer

---
+++ Project Neptune/Assets/Rotater.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+    public int speed = 5;
+    Rigidbody Rb;
+   public Vector3 rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotation * Time.deltaTime);
+    }
+}
