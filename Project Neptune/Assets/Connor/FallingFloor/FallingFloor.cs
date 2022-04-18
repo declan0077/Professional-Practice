37b44d265575610b0575b2b21fd0d3f1dfda103c
2munroc <s208378@uos.ac.uk>
2022-04-18 14:41:52 +0100

    added falling floor puzzle
    something up with gravity, will research how to fix

---
+++ Project Neptune/Assets/Connor/FallingFloor/FallingFloor.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingFloor : MonoBehaviour
+{
+    bool isFalse;
+    Renderer myRend;
+
+    [SerializeField] FallingFloor[] group;
+    Rigidbody myRB;
+
+    [SerializeField] bool UnvealPath;
+
+    private void OnEnable()
+    {
+        myRend = GetComponent<Renderer>();
+        myRB = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        if (group.Length == 0)
+            return;
+
+        int pathIndex = Random.Range(0, group.Length);
+        for (int i = 0; i < group.Length; i++)
+        {
+            if (pathIndex == i)
+            {
+                group[i].SetPath();
+            }
+            else
+            {
+                group[i].SetFalsePath();
+            }
+
+        }
+
+    }
+    public void SetPath()
+    {
+        isFalse = false;
+        myRB.isKinematic = true;
+        if (UnvealPath)
+        {
+
+        }
+    }
+    public void SetFalsePath()
+    {
+        isFalse = true;
+        myRB.isKinematic = false;
+        if (UnvealPath)
+        {
+
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "player")
+        {
+            if (isFalse)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
