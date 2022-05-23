851c175f66aa01a47c8b585246b6d0b3d1c72843
declan0077 <icetheicebear@gmail.com>
2022-05-23 15:41:54 +0100

    Trail render - Level1 more boxes

--- Project Neptune/Assets/Scripts/PlayerNameAboveHead.cs
+++ Project Neptune/Assets/Scripts/PlayerNameAboveHead.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 using Photon.Pun;
 using UnityEngine.UI;
 using TMPro;
+
 public class PlayerNameAboveHead : MonoBehaviour
 {
  public TextMeshPro Name;
         private void Start()
     {
+
         Name.text = PhotonNetwork.NickName;
     }
 
