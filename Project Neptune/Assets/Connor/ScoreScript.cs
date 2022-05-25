03aa7ce98b911867fce9039709406324a3f93480
declan0077 <icetheicebear@gmail.com>
2022-05-25 04:23:17 +0100

    Lobby and Netball level upgrade

--- Project Neptune/Assets/Connor/ScoreScript.cs
+++ Project Neptune/Assets/Connor/ScoreScript.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class ScoreScript : MonoBehaviour
 {
-    public Text MyScoretext;
+    public TextMeshPro MyScoretext;
     private int ScoreNum;
     public GameObject Wall;
+    private ParticleSystem PS;
+    private AudioSource AS;
 
     // Start is called before the first frame update
     void Start()
     {
         ScoreNum = 0;
-        MyScoretext.text = "Score" + ScoreNum;
+        MyScoretext.text = "Hoops Scored " + ScoreNum;
+        PS = GetComponent<ParticleSystem>();
+        AS = GetComponent<AudioSource>();
     }
     private void Update()
     {
@@ -31,8 +35,8 @@ public class ScoreScript : MonoBehaviour
         {
             ScoreNum += 2;
             MyScoretext.text = "Score" + ScoreNum;
-
-       
+            PS.Play();
+            AS.Play();
 
         }
        
